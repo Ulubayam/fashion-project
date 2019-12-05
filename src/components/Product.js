6462f6736ddccdf2e21d98f4e8d119fd0fc7a53a
Ulubayam <gizemulubayam@gmail.com>
2019-12-05 21:23:07 +0300

    fixed header

--- src/components/Product.js
+++ src/components/Product.js
@@ -11,7 +11,7 @@ export const Card = styled.article`
   width: 263pt;
   max-height: 390pt;
   flex: 0 0 200px;
-  margin: 20px;
+  margin: 50px;
   border: 1px solid #ccc;
   box-shadow: 2px 2px 6px 0px rgba(0, 0, 0, 0.3);
 `;
@@ -20,13 +20,47 @@ export const Image = styled.img`
 `;
 export const Text = styled.div`
   padding: 0 20px 20px;
+  font-size: 16pt;
+  line-height: 25pt;
 `;
+export const Price = styled.div`
+padding: 0 20px 0px;
+font-size:14pt;
+line-height:30pt;
+`
 export const Section = styled.section`
   margin: 0 1.5% 24px 1.5%;
   margin-top: 100pt;
   float: right;
   width: 63%;
 `;
+export const New = styled.span`
+position: absolute;
+right:-20px;
+top:10px;
+background:red;
+text-align: center;
+border-radius: 30px 30px 30px 30px;
+color:white;
+padding:5px 10px;
+font-size:20px;
+`
+export const Discount = styled.span`
+position: absolute;
+left:-20px;
+top:10px;
+background:blue;
+text-align: center;
+border-radius: 30px 30px 30px 30px;
+color:white;
+padding:5px 10px;
+font-size:20px;
+`
+export const Div = styled.div`
+position:relative;
+padding-top:20px;
+display:inline-block;
+`
 
 export default class Product extends React.Component {
   constructor(props) {
@@ -37,10 +71,11 @@ export default class Product extends React.Component {
   }
 
   getData = () => {
-    const apiBaseUrl = "https://cimri.free.beeceptor.com";
+    const apiBaseUrl = "https://deneme.free.beeceptor.com";
     const endpoint = "/products";
     axios.get(`${apiBaseUrl}${endpoint}`).then(response => {
       const products = response.data;
+      console.log(products)
       this.setState({ products });
     });
   };
@@ -52,18 +87,28 @@ export default class Product extends React.Component {
   render() {
     return (
       <Section>
-        <Cards>
-          {this.state.products.map((product, i) => {
+          <Cards>
+          {
+          this.state.products.map((item, i) => {
             return (
               <Card key={i}>
-                <Image src={product.products.imageUrl} />
+                <Div>
+                {
+                  item.products.badges.map((badge,i) => {
+                   return  badge === "new" ? <New key={i}>New</New> : <Discount key={i}>Discount</Discount>
+                  })
+                }
+                <Image src={item.products.imageUrl} />
+                </Div>
                 <Text>
-                 {product.products.name}
+                 {item.products.name}
                 </Text>
+                <Price> ${item.products.price}</Price>
               </Card>
-            );
-          })}
-        </Cards>
+            )
+          })
+          }
+        </Cards>  
       </Section>
     );
   }
