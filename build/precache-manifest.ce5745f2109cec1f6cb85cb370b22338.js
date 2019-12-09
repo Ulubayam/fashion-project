e0f3039d624453de10eb1c0b48dc188cefe07a89
gizemu1 <GizemU1@verifone.com>
2019-12-09 14:05:05 +0300

    gh-pages

---
+++ build/precache-manifest.ce5745f2109cec1f6cb85cb370b22338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bca51aa3a777e76a50644572a4237b5",
+    "url": "/fashion-project/index.html"
+  },
+  {
+    "revision": "7ab1d634710c8dd2adf3",
+    "url": "/fashion-project/static/css/main.02379119.chunk.css"
+  },
+  {
+    "revision": "bd52b697c2a6e31477f0",
+    "url": "/fashion-project/static/js/2.f7eabfbb.chunk.js"
+  },
+  {
+    "revision": "7ab1d634710c8dd2adf3",
+    "url": "/fashion-project/static/js/main.05998146.chunk.js"
+  },
+  {
+    "revision": "771d83864cff69c3f17b",
+    "url": "/fashion-project/static/js/runtime-main.812a83a0.js"
+  }
+]);
