a5d6b9f955469a305af599ac1c269c08e3d6d4ef
gizemu1 <GizemU1@verifone.com>
2019-12-09 14:13:04 +0300

    Updates

---
+++ precache-manifest.edf63c159e09fe9d5e810608ec0855bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1758b156fb0ee1adba07701d4477f1",
+    "url": "/fashion-project/index.html"
+  },
+  {
+    "revision": "41a28cd5904b3d35862d",
+    "url": "/fashion-project/static/css/main.02379119.chunk.css"
+  },
+  {
+    "revision": "2908c7bf1d7accac19ea",
+    "url": "/fashion-project/static/js/2.2ba500ae.chunk.js"
+  },
+  {
+    "revision": "41a28cd5904b3d35862d",
+    "url": "/fashion-project/static/js/main.386ffd81.chunk.js"
+  },
+  {
+    "revision": "256547a60e6dcb3281a6",
+    "url": "/fashion-project/static/js/runtime-main.474993ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cimri-project/precache-manifest.28b534ec351b8de15ebafdfec85940a1.js"
+  "/fashion-project/precache-manifest.edf63c159e09fe9d5e810608ec0855bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cimri-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fashion-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
